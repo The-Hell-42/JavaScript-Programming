063dac302de228f3fd32c0668de578225661bec6
The-Hell-42 <sahsiddhartha19@gmail.com>
2023-09-30 13:13:17 -0700

    Finally i completed Chapter-5 which ws left yesterday(Date:09/29/2023)

--- Chapter-5.js
+++ Chapter-5.js
@@ -12,9 +12,9 @@
 // console.log(mark_ofclass_12[6]);
 // console.log(mark_ofclass_12[7]);
 // console.log(mark_ofclass_12[8]);
-// console.log("The value of 9th index in an array is ", mark_ofclass_12[9]); //it will be undefined because index 9 is not present
-// mark_ofclass_12[0] = 24;  //changing the value of an array
-// mark_ofclass_12[9] = 99; //Adding a new value to an array
+// console.log("The value, index, array of 9th index in an array is ", mark_ofclass_12[9]); //it will be undefined because index 9 is not present
+// mark_ofclass_12[0] = 24;  //changing the value, index, array of an array
+// mark_ofclass_12[9] = 99; //Adding a new value, index, array to an array
 // console.log(mark_ofclass_12);
 // console.log(typeof mark_ofclass_12);
 
@@ -69,29 +69,101 @@
 
 // For  loop in arrays
 
-let num2 = [1, 2, 3, 4, 5, 6, 7];
-for (let i = 0; i < num2.length; i++) {
-    console.log(num2[i]);
-}
-// Shortcut of for loop in array
-// for ... in loop
-for(let i in num2){
-    console.log(i);
-}
-// for ..of loop
-for(let item of num2){
-    console.log(item);
-}
+// let num2 = [1, 2, 3, 4, 5, 6, 7];
+// for (let i = 0; i < num2.length; i++) {
+//     console.log(num2[i]);
+// }
+// // Shortcut of for loop in array
+// // for ... in loop
+// for (let i in num2) {
+//     console.log(i);
+// }
+// // for ..of loop
+// for (let item of num2) {
+//     console.log(item);
+// }
 
 
-// For each loop
+// // For each loop
 
-num2.forEach((element)=>{
-    console.log(element*element);
-})
+// num2.forEach((element) => {
+//     console.log(element * element);
+// })
 
 // Array.from loop
 
-let naam = "Anurag";
-let arr = Array.from(naam);
-console.log(arr);
+// let naam = "Anurag";
+// let arr = Array.from(naam);
+// console.log(arr);
+
+// Array Map Method in java script
+
+// let arr = [12, 13, 14, 15, 16];
+// let a = arr.map((value, index, array) => {
+//     console.log(value, index, array);
+//     return value + index ;
+// });
+
+// console.log(a);
+
+// Array filter Method in java script
+
+// let arr2 = [1,2,3,4,5,12,13,14,15,21,22,23,24,25];
+// let a2 = arr2.filter((a)=>{
+//     return a < 10;
+// })
+// console.log(a2);
+
+// let arr2 = [1, 2, 3, 4, 5, 12, 13, 14, 15, 21, 22, 23, 24, 25];
+// let a = arr2.reduce((h1, h2) => {
+//     console.log(h1 - h2)
+//     return h1 - h2;
+// })
+// console.log(a)
+
+
+// Chapter-5 practise question
+// Problem-No-1
+
+// let arr = [1,2,3,4,5,6];
+//  let a = prompt("Enter a number "); //Here the prompt will not work and it only works in browser 
+// a = Number.parseInt(a);
+// arr.push(a);
+// console.log(a); 
+
+// Problem-No-2
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let a;
+// do {
+//     a = prompt("Enter a number "); //Here the prompt will not work and it only works in browser 
+//     a = Number.parseInt(a);
+//     arr.push(a);
+// }while(a!=0)
+// console.log(a); 
+
+// Problem-No-3
+
+// let arr = [10, 20, 30, 4, 50, 60, 70]
+// let a = arr.filter((x)=>{
+//     return x%10 == 0;
+// })
+// console.log(a)
+
+
+// Problem-No-4
+
+// let arr = [10, 20, 30, 4, 50, 60, 70]
+// let a = arr.map((x)=>{
+//     return x*x;
+// })
+// console.log(a)
+
+
+// Problem-No-5
+let arr = [1,2,3,4,5]
+let a = arr.reduce((x1, x2,x3)=>{
+    console.log(x1*x2+x3)
+    return x1*x2+x3;
+})
+console.log(a)
